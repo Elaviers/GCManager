4dde20f2c5982703357739e62ee84de8e12780be
Elaviers <elaviers@gmail.com>
2019-04-27 23:11:38 -0700

    Make config reader actually use DownloadDirectory

--- GCManager/ManagerInfo.cs
+++ GCManager/ManagerInfo.cs
@@ -9,7 +9,7 @@ namespace GCManager
     {
         public string installDir = null;
 
-        public string DownloadsFolderName = null;
+        public string DownloadDirectory = null;
 
         private static ManagerInfo _instance = null;
 
@@ -17,7 +17,7 @@ namespace GCManager
         {
             if (_instance == null)
             {
-                string configFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.cfg");
+                string configFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
 
                 if (File.Exists(configFileName))
                 {
@@ -40,16 +40,19 @@ namespace GCManager
                     }
                 }
 
+                if (_instance.DownloadDirectory == null)
+                    _instance.DownloadDirectory = "Downloads";
+
                 try
                 {
                     File.WriteAllText(configFileName, JsonConvert.SerializeObject(_instance));
                 }
                 catch (IOException e)
                 {
-                    System.Windows.MessageBox.Show("config write error" + e.ToString());
+                    System.Windows.MessageBox.Show("config write error " + e.ToString());
                 }
 
-                _instance.DownloadsFolderName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Downloads");
+                _instance.DownloadDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _instance.DownloadDirectory);
             }
 
             return _instance;
